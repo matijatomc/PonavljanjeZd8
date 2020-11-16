2df409e37b4259a29308c5ded22cc05aae7c3d2e
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-11-17 00:11:55 +0100

    Add files via upload

---
+++ tomcMatijaPonavljanje8/tomcMatijaPonavljanje8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaPonavljanje8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sport Curling = new Sport("Curling");
+            Sport Vaterpolo = new Sport("Vaterpolo", true);
+            Sport Nogomet = new Sport("Nogomet", true, 11);
+
+        }
+    }
+}
